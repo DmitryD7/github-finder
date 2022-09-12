df496e331a15f543f7618e4bd6ae94a67efb6164
DmitryDemid <dimon.demid15@gmail.com>
2022-09-11 21:55:28 -0400

    implementing feature to add repo to favourites

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import {IRepo} from "../models/models";
+import {useActions} from "../hooks/actions";
+import {useAppSelector} from "../hooks/redux";
 
 export function RepoCard({repo}: { repo: IRepo }) {
+    const {addToFavourites, removeFromFavourites} = useActions();
+    const {favourites} = useAppSelector(state => state.github);
+
+    const [isFav, setIsFav] = useState(favourites.includes(repo.html_url));
+
+    const addToFavouritesHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        addToFavourites(repo.html_url);
+        setIsFav(true);
+    };
+
+    const removeFromFavouritesHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        removeFromFavourites(repo.html_url);
+        setIsFav(false);
+    };
+
     return (
         <div className={'border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all'}>
             <a href={repo.html_url} target={'_blank'}>
@@ -11,6 +30,16 @@ export function RepoCard({repo}: { repo: IRepo }) {
                     Watchers: <span className={'font-bold'}>{repo.watchers}</span>
                 </p>
                 <p className={'text-sm font-thin'}>{repo?.description}</p>
+
+                {!isFav && <button
+                    className={'py-2 px-4 bg-yellow-400 rounded hover:shadow-md transition-all'}
+                    onClick={addToFavouritesHandler}
+                >Add</button>}
+
+                {isFav && <button
+                    className={'py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all'}
+                    onClick={removeFromFavouritesHandler}
+                >Remove</button>}
             </a>
         </div>
     );
