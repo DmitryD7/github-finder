a02798bd465bc365159118426bc944b21163e01b
DmitryDemid <dimon.demid15@gmail.com>
2022-09-11 21:09:03 -0400

    implementing user repos searching

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,22 +1,28 @@
 import React, {useEffect, useState} from "react";
-import {useSearchUsersQuery} from "../../store/github/github.api";
+import {useLazyGetUserReposQuery, useSearchUsersQuery} from "../../store/github/github.api";
 import {useDebounce} from "../../hooks/debounce";
+import {RepoCard} from "../../components/RepoCard";
 
 export function HomePage() {
     const [search, setSearch] = useState('');
     const [isDroppedDown, setIsDroppedDown] = useState(false);
     const debounced = useDebounce(search);
+
     const {isLoading, isError, data} = useSearchUsersQuery(debounced, {
         skip: debounced.length < 3,
         refetchOnFocus: true,
     });
+    const [fetchRepos, {isLoading: areReposLoading, data: repos}] = useLazyGetUserReposQuery();
+
 
     useEffect(() => {
         setIsDroppedDown(debounced.length > 3 && data?.length! > 0)
     }, [debounced, data]);
 
     const onUserClickHandler = (username: string) => {
-
+        fetchRepos(username);
+        setSearch('');
+        setIsDroppedDown(false);
     };
 
     return (
@@ -43,6 +49,12 @@ export function HomePage() {
                         >{user.login}</li>
                     ))}
                 </ul>}
+
+                <div className="container">
+                    {areReposLoading && <p className={'text-center'}>Repos are loading...</p>}
+
+                    {repos?.map(repo => <RepoCard repo={repo} key={repo.id}/>)}
+                </div>
             </div>
         </div>
     );
